1e554b2955de2d62b4c28720b34623bf6cb69b85
avc0021 <avc0021@gmail.com>
2022-01-15 14:37:43 -0600

    added modules to import on app file

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+
+// Import modules from library
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
 
 inquirer
     .prompt([
         {
             type:'text',
             name: 'mname',
-            message:'Enter team managers name'
+            message:'Enter team managers name',
         },
         {
             type: 'number',
             name: 'id',
-            message: 'Enter managers ID'
+            message: 'Enter managers ID',
         },
         {
             type: 'text',
             name: 'email',
-            message: 'Enter managers email' 
+            message: 'Enter managers email',
         },
         {
             type: 'number',
             name: 'officenumber',
-            message: 'Enter managers office number'
-        }
+            message: 'Enter managers office number',
+        },
         {
-            
+            type: 'list',
+            message: 'What is your role?',
+            name: 'role',
+            choices: ['Manager', 'Engineer', 'Intern'],
         }
     ])
