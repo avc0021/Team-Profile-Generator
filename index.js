6929a60e7704216798a16a4a600e5914dfba1cb9
avc0021 <avc0021@gmail.com>
2022-01-16 15:08:20 -0600

    other two conditionals added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 
-//ask questions
+//ask general questions first and onces entered it will jump to details questions
 const detailedQs = async() => {
   const generalQs = await inquirer
         .prompt([
@@ -33,8 +33,9 @@ const detailedQs = async() => {
                 choices: ['Manager', 'Engineer', 'Intern'],
             }
         ])
-        console.log(generalQs);
-        if (answers.role === 'Engineer') {
+
+       // if manager is selected enter office number
+        if (generalQs.role === 'Manager') {
             const managerInfo = await inquirer
             .prompt([
                 {
@@ -43,17 +44,56 @@ const detailedQs = async() => {
                     message: 'Enter managers office number'
                 },
             ])
+        // creates new manager object if manager is selected
             const newLeader = new Manager(
-                answers.name,
-                answers.id,
-                answers.email,
+                generalQs.name,
+                generalQs.id,
+                generalQs.email,
                 managerInfo.officeNumber
             );
+            newEmployeeData.push(newLeader);
+        
+        // if engineer is seleceted as a role answer these questions
+        } else if (generalQs.role === 'Engineer') {
+            const engineerInfo = await inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'ghusername',
+                    message: 'Enter github username'
+                },
+            ])
+        // creates new engineer object using data provided
+            const newEngineer = new Engineer(
+                generalQs.name,
+                generalQs.id,
+                generalQs.email,
+                generalQs.ghUsername
+            );
+            newEmployeeData.push(newEngineer);
+        
+        // if intern is selected ask these questions
+        } else if (generalQs.role === 'Intern') {
+            const internInfo = await inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'school',
+                    message: 'Enter school attended'
+                },
+            ])
+        // creates a intern object with data entered
+            const newIntern = new Intern(
+                generalQs.name,
+                generalQs.id,
+                generalQs.email,
+                generalQs.school
+            );
         }
-
+    };  
         // ask for office number if manager
         // ask for github username if  engineer
         // ask for school if intern
-    };    
+    
 
         // collect data and write file to html
